4fbdd4272672973aeb98545f1fcf1890738555ee
wurui <wuruiwm@qq.com>
2018-12-22 17:07:44 +0800

    搜索改为只有地区是必填项，其他都是选填，并且地址和小区名字的搜索分开

--- app/api/controller/v1/Housesearch.php
+++ app/api/controller/v1/Housesearch.php
@@ -8,18 +8,17 @@ class Housesearch extends Controller{
      * @apiVersion 0.1.0
      * @apiDescription  首页模糊搜索
      * @apiSampleRequest http://estate.dingdingmaoer.cn/api/v1/House/search
-     * @apiParam {string} search 搜索关键词  
+     * @apiParam {string} search 房源名称
+     * @apiParam {string} address 地址
      * @apiParam {int} city 市的ID 
      * @apiParam {int} floor_area_min 面积最小值
      * @apiParam {int} floor_area_max 面积最大值
-     * @apiParam {int} init_status 房屋类型  1新房 2二手房 
+     * @apiParam {int} init_status 房屋类型  1新房 2二手房
      */
 	public function search(){
 		//获取搜索关键词
 		$search = input('get.search');
-		if (!$search) {
-			return ['reg'=>'查询关键词不能为空'];
-		}
+		$address = input('get.address');
 		$str = '1';
 		//获取传来的市的ID,拼接成where语句
 		$city = input('get.city');
@@ -39,46 +38,30 @@ class Housesearch extends Controller{
 				//实例化model并执行查询
 				//先执行进行标题查询
 				$house = model('HouseSource');
-				$res = $house->field(['id','title','house_price','house_address','decoration_type','init_status'])->where($str)->where('floor_area','between',[$floor_area_min,$floor_area_max])->where('title','like',"%$search%")->select();
-				$data1 = [];
+				$res = $house->field(['id','title','house_price','house_address','decoration_type','init_status'])->where($str)->where('floor_area','between',"$floor_area_min,$floor_area_max")->where('title','like',"%$search%")->where('house_address','like',"%$address%")->select();
+				$data = [];
 				foreach ($res as $k => $v) {
-					$data1[] = $v->getData();
+					$data[] = $v->getData();
 				}
-				//再执行地址查询
-				$res = $house->field(['id','title','house_price','house_address','decoration_type','init_status'])->where($str,$city)->where('floor_area','between',[$floor_area_min,$floor_area_max])->where('house_address','like',"%$search%")->select();
-				$data2 = [];
-				foreach ($res as $k => $v) {
-					$data2[] = $v->getData();
-				}
-				//合并两个查询结果
-				$data3 = array_merge($data1,$data2);
 				//判断是否有数据，没有则说明没有查到结果，返回提示信息，有结果则把数组return出去
-				if(!$data3){
+				if(!$data){
 					return ['reg'=>'关键词没有查到结果，建议更换关键词'];
 				}
-				return $data3;
+				return $data;
 		}
 		//实例化model并执行查询
 		//先执行进行标题查询
 		$house = model('HouseSource');
-		$res = $house->field(['id','title','house_price','house_address','decoration_type','init_status'])->where($str)->where('title','like',"%$search%")->select();
-		$data1 = [];
+		$res = $house->field(['id','title','house_price','house_address','decoration_type','init_status'])->where($str)->where('title','like',"%$search%")->where('house_address','like',"%$address%")->select();
+		$data = [];
 		foreach ($res as $k => $v) {
-			$data1[] = $v->getData();
+			$data[] = $v->getData();
 		}
-		//再执行地址查询
-		$res = $house->field(['id','title','house_price','house_address','decoration_type','init_status'])->where($str,$city)->where('house_address','like',"%$search%")->select();
-		$data2 = [];
-		foreach ($res as $k => $v) {
-			$data2[] = $v->getData();
-		}
-		//合并两个查询结果
-		$data3 = array_merge($data1,$data2);
 		//判断是否有数据，没有则说明没有查到结果，返回提示信息，有结果则把数组return出去
-		if(!$data3){
+		if(!$data){
 			return ['reg'=>'关键词没有查到结果，建议更换关键词'];
 		}
-		return $data3;
+		return $data;
 	}
 }
 
--- public/apidoc/api_project.js
+++ public/apidoc/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-12-22T02:27:06.675Z",
+    "time": "2018-12-22T09:07:06.583Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
