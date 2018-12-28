37ada00ec2bc47c539f16c221c65803c34c1bef7
wurui <wuruiwm@qq.com>
2018-12-28 17:51:02 +0800

    根据传来省市区的id获取报备楼盘，增加判断，如果传来的区县id查出来的name为市辖区，那么就把这个市所有的楼盘都丢给前端，但是客户不知道自己要不要，先注释！！！

--- app/api/controller/v1/Loupan.php
+++ app/api/controller/v1/Loupan.php
@@ -17,20 +17,40 @@ class Loupan extends controller{
 		if (!(is_numeric($province)&&is_numeric($city)&&is_numeric($area))) {
 			return ['reg'=>'请输入正确的省,市,区县ID'];
 		}
-		$house = model('HouseSource');
-		$res = $house->field(['id','title'])->where('province',$province)->where('city',$city)->where('area',$area)->select();
-		$data = [];
-		foreach ($res as $k => $v) {
-			$data[] = $v->getData();
-		}
-		if (!$data) {
-			return ['reg'=>'暂无房源信息'];
-		}else {
-			return $data;
-		}
+		// $area_model = model('Area');
+		// $area_res = $area_model->where('_id',$area)->find()->getData();
+		// if ($area_res['name'] == '市辖区') {
+		// 	$house = model('HouseSource');
+		// 	$res = $house->field(['id','title'])->where('province',$province)->where('city',$city)->select();
+		// 	$data = [];
+		// 	foreach ($res as $k => $v) {
+		// 		$data[] = $v->getData();
+		// 	}
+		// 	if (!$data) {
+		// 		return ['reg'=>'暂无房源信息'];
+		// 	}else {
+		// 		return $data;
+		// 	}
+		// }else{
+			$house = model('HouseSource');
+			$res = $house->field(['id','title'])->where('province',$province)->where('city',$city)->where('area',$area)->select();
+			$data = [];
+			foreach ($res as $k => $v) {
+				$data[] = $v->getData();
+			}
+			if (!$data) {
+				return ['reg'=>'暂无房源信息'];
+			}else {
+				return $data;
+			}
+	  	// }
 	}
 }
 
 
 
  ?>
+
+
+
+
