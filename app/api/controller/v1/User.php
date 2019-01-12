db57ce3ebccceb53ec13bed5af8fe3d984ebf13d
iActing <758246061@qq.com>
2019-01-12 12:07:58 +0800

    新增手机号码段正则

--- app/api/controller/v1/User.php
+++ app/api/controller/v1/User.php
@@ -94,13 +94,12 @@ class User extends BaseController
                 'head_img' => $head_img_path
             ]);
         if ($result) {
-
-
             $image = @ImageCreateFromJpeg($head_img_path);
             if (!$image)
             {
                 $image= imagecreatefromstring(file_get_contents($head_img_path));
             }
+
             $exif = exif_read_data($head_img_path);     //注意先在php.ini中开启exif扩展
             if(!empty($exif['Orientation'])) {
                 switch($exif['Orientation']) {
